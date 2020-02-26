394412b4ac047ad5a9d4b694d3900300343146f9
catalpeak <784734297@163.com>
2020-02-26 15:02:02 +0800

    2020-2-26-0.0.0.1 Modify Main Scene

--- assets/Script/MainJS/MainOpitionButton.js
+++ assets/Script/MainJS/MainOpitionButton.js
@@ -1,18 +1,48 @@
 
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
-
+        BG : {
+            default : null,
+            type : cc.Node
+        },
+        Label : {
+            default : null,
+            type : cc.Label
+        },
     },
 
     onLoad () {
-        let windowSize=cc.view.getVisibleSize();
-        this.node.setPosition (windowSize.width / 7 * 2, -60, 0);
+        let windowSize=cc.view.getVisibleSize();  // 获取手机窗口大小
+        this.node.setPosition (windowSize.width / 7 * 2, -110, 0); // 设置游戏按钮位置
+
+        this.BG.opacity = 0;
+        this.Label.node.color = cc.color (255, 255, 255, 255); // 初始化按钮节点
+
+        this.node.on ("touchstart", this.OnTouchStart, this);
+        this.node.on ("touchcancel", this.OnTouchCancel, this);
+        this.node.on ("touchend", this.OnTouchEnd, this);
     },
 
     start () {
 
+    },
+
+    OnTouchStart () {
+        this.BG.opacity = 100;
+        this.Label.node.color = cc.color (0, 0, 0, 0);
+    },
+
+    OnTouchCancel () {
+        this.BG.opacity = 0;
+        this.Label.node.color = cc.color (255, 255, 255, 255);
+    },
+
+    OnTouchEnd () {
+        this.BG.opacity = 0;
+        this.Label.node.color = cc.color (255, 255, 255, 255);
     },
 
     // update (dt) {},
